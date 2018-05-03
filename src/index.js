97174a71f9f6aa4a93cba8387d034c541fdeca29
Satyendra Singh <satyendra1529singh@gmail.com>
2018-05-04 03:03:30 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class PhoneCode extends React.Component {
+    render() {
+        return (
+            <div>Phone Code Component</div>
+        );
+    }
+}
+export default PhoneCode;
